39d61819237d87146692ca4fb76ddae902b04071
natagw <satyabilyasa@gmail.com>
2024-11-08 13:22:34 +0700

    update histori transaksi

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -121,7 +121,7 @@ class TransaksiController extends Controller
         $user = Auth::user()->id;
         return view('dashboard.histori-transaksi.index',[
             'title' => 'Histori Transaksi',
-            'transaksis' => Transaksi::where('reseller_id', $user)->paginate(10),
+            'transaksis' => Transaksi::with('product')->where('reseller_id', $user)->paginate(10),
             'transaksiProdusen' => Transaksi::where('produsen_id', $user)->paginate(10)
         ]);
     }
